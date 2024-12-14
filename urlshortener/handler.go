aeb1eb75e852e640b585012038e9c7492c73113c
John <26047888@sun.ac.za>
2024-12-14 16:17:28 +0200

    added handler class

---
+++ urlshortener/handler.go
@@ -0,0 +1,16 @@
+package urlshortener
+
+import (
+	"net/http"
+)
+
+func mapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		url := pathsToUrls[req.URL.Path]
+		if url != "" {
+			http.Redirect(res, req, url, http.StatusPermanentRedirect)
+		} else {
+			fallback.ServeHTTP(res, req)
+		}
+	})
+}
