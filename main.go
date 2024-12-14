97e256490ec083796d90f4778aa777b2d3d1bf1a
John <26047888@sun.ac.za>
2024-12-14 16:48:52 +0200

    functioning handler

--- main.go
+++ main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/Jhone-Paul/UrlShortener/urlshortener"
+)
+
+func main() {
+	// These are the URLS to be mapped
+	pathsToUrls := map[string]string{
+		"/git": "https://github.com/Jhone-Paul",
+		"/go":  "https://golang.org",
+	}
+
+	fallback := http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		res.WriteHeader(http.StatusNotFound)
+		res.Write([]byte("404 - Not Found"))
+	})
+
+	handler := urlshortener.MapHandler(pathsToUrls, fallback)
+	//you can select your own port :0
+	port := ":8080"
+	println("Starting the server on", port)
+	err := http.ListenAndServe(port, handler)
+	if err != nil {
+		panic(err)
+	}
+}
