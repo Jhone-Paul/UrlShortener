04bfca953dda80881b0212f4fc10076cab6bfbeb
John <26047888@sun.ac.za>
2024-12-17 21:36:55 +0200

    added .json input file functionality

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
+
+	"log"
+	"os"
 
 	"github.com/Jhone-Paul/UrlShortener/urlshortener"
 )
 
 func main() {
-	// These are the URLS to be mapped
-	pathsToUrls := map[string]string{
-		"/git": "https://github.com/Jhone-Paul",
-		"/go":  "https://golang.org",
+	pathsToUrls, err := loadMappings("mappings.json")
+	if err != nil {
+		log.Fatalf("Failed to load mappings: %v", err)
 	}
 
 	fallback := http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
@@ -19,11 +23,33 @@ func main() {
 	})
 
 	handler := urlshortener.MapHandler(pathsToUrls, fallback)
-	//you can select your own port :0
+
 	port := ":8080"
 	println("Starting the server on", port)
-	err := http.ListenAndServe(port, handler)
+	err = http.ListenAndServe(port, handler)
 	if err != nil {
 		panic(err)
 	}
+}
+
+// loadMappings reads a JSON file and parses it into a map
+func loadMappings(filename string) (map[string]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	byteValue, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	var pathsToUrls map[string]string
+	err = json.Unmarshal(byteValue, &pathsToUrls)
+	if err != nil {
+		return nil, err
+	}
+
+	return pathsToUrls, nil
 }
